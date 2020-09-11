b280e40756fa1e2e76dc6d435c04da8aa42a4b1f
Max <example@gmail.com>
2020-09-11 15:40:45 +0500

    Second stage

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 "use strict";
 
-
 const numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", '');
-const lastFilm = prompt("Один из последних просмотренных фильмов?"),
-      rating = prompt("На сколько оцените его?"),
-      lastFilm2 = prompt("Один из последних просмотренных фильмов?"),
-      rating2 = prompt("На сколько оцените его?");
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -15,7 +10,27 @@ const personalMovieDB = {
     privat: false
 };
 
-console.log(personalMovieDB);
+for (let i = 0; i < 2; i++) {
+    const lastFilm = prompt("Один из последних просмотренных фильмов?", ''),
+          rating = prompt("На сколько оцените его?", '');
 
-personalMovieDB.movies[lastFilm] = rating;
-personalMovieDB.movies[lastFilm2] = rating2;
+    if (lastFilm != null && rating != null && lastFilm != '' && rating != '' && lastFilm.length <= 50) {
+        personalMovieDB.movies[lastFilm] = rating;
+        console.log('done');
+    } else { 
+        alert('В названии фильма не может быть 0 или более 50 символов');
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count >= 0 && personalMovieDB.count < 10 ) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    alert('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+    alert ('Вы киноман');
+} else { 
+    alert('Произошла ошибка');
+}
+console.log(personalMovieDB);
